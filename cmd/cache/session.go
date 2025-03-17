6e41e7db8c1a4f808859aea6ac954842617d6cf7
Hong Anh Pham <phamhonganh12062000@gmail.com>
2025-03-17 09:49:53 +0700

    done redis clone as separate service

--- cmd/cache/session.go
+++ cmd/cache/session.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"time"
 
 	"greenlight.honganhpham.net/internal/logger"
 )
@@ -44,13 +45,13 @@ func startSession(conn net.Conn, logger *logger.Logger) {
 
 func (cmd Command) handle(logger *logger.Logger) bool {
 	switch strings.ToUpper(cmd.args[0]) {
-	case "GET":
+	case GET:
 		return cmd.get(logger)
-	// case "SET":
-	// 	return cmd.set(logger)
-	case "DEL":
+	case SET:
+		return cmd.set(logger)
+	case DEL:
 		return cmd.del()
-	case "QUIT":
+	case QUIT:
 		return cmd.quit(logger)
 	default:
 		logger.Info("Command not supported", map[string]string{"command": cmd.args[0]})
@@ -100,4 +101,80 @@ func (cmd *Command) get(logger *logger.Logger) bool {
 		cmd.conn.Write([]uint8("$-1\r\n"))
 	}
 	return true
+}
+
+func (cmd *Command) set(logger *logger.Logger) bool {
+	if len(cmd.args) < 3 || len(cmd.args) > 6 {
+		cmd.conn.Write([]uint8("-ERR wrong number of arguments for '" + cmd.args[0] + "' command\r\n"))
+		return true
+	}
+	logger.Info("Handle SET", nil)
+	logger.Info("Value length", map[string]string{"length": strconv.Itoa(len(cmd.args[2]))})
+	if len(cmd.args) > 3 {
+		pos := 3
+		option := strings.ToUpper(cmd.args[pos])
+		switch option {
+		// Set the key if it does not exist before
+		case NX:
+			logger.Info("Handle NX", nil)
+			if _, ok := cache.Load(cmd.args[1]); ok {
+				cmd.conn.Write([]uint8("$-1\r\n"))
+				return true
+			}
+			pos++
+		// Only set the key if it it already exists
+		case XX:
+			logger.Info("Handle NX", nil)
+			if _, ok := cache.Load(cmd.args[1]); !ok {
+				cmd.conn.Write([]uint8("$-1\r\n"))
+				return true
+			}
+			pos++
+		}
+
+		// Parse the expiration flag
+		if len(cmd.args) > pos {
+			if err := cmd.setExpiration(pos, logger); err != nil {
+				cmd.conn.Write([]uint8("-ERR " + err.Error() + "\r\n"))
+				return true
+			}
+		}
+
+	}
+
+	// Set the value
+	cache.Store(cmd.args[1], cmd.args[2])
+	cmd.conn.Write([]uint8("+OK\r\n"))
+	return true
+}
+
+func (cmd *Command) setExpiration(pos int, logger *logger.Logger) error {
+	option := strings.ToUpper(cmd.args[pos])
+	value, _ := strconv.Atoi(cmd.args[pos+1])
+	var duration time.Duration
+
+	switch option {
+	case EX:
+		duration = time.Second * time.Duration(value)
+	case PX:
+		duration = time.Millisecond * time.Duration(value)
+	default:
+		return fmt.Errorf("expiration option not valid")
+	}
+
+	// Wait by sleeping then delete the key-value from the cache
+	go func() {
+		logger.Info("Handling expirations", map[string]string{"option": option, "duration": shortDur(duration)})
+		time.Sleep(duration)
+		cache.Delete(cmd.args[1])
+	}()
+	return nil
+}
+
+func shortDur(d time.Duration) string {
+	s := d.String()
+	if strings.HasSuffix(s, "m0s") || strings.HasSuffix(s, "h0m") {
+		s = s[:len(s)-2]
+	}
+	return s
 }
